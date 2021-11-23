a167b1889c561a8d9555da279b42a1c405e9af68
田 知勲 <j-jihoon@winas.jp>
2021-11-23 19:24:21 +0900

    공지사항 뷰컨트롤러 추가

---
+++ Firebase_Notice/Firebase_Notice/NoticeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NoticeViewController.swift
+//  Firebase_Notice
+//
+//  Created by 전지훈 on 2021/11/23.
+//
+
+import UIKit
+
+class NoticeViewController: UIViewController {
+    
+    var noticeContents: (title: String, detail: String, date: String)?
+
+    @IBOutlet weak var noticeView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        noticeView.layer.cornerRadius = 6
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        
+        guard let noticContents = noticeContents else {return}
+        
+        titleLabel.text = noticContents.title
+        detailLabel.text = noticContents.detail
+        dateLabel.text = noticContents.date
+    }
+    
+    
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Firebase_Notice/Firebase_Notice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase_Notice
+//
+//  Created by 전지훈 on 2021/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
